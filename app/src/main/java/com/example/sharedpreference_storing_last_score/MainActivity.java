604be5786cfc65c59c3b68e37c604689f7bc69ec
Md Mehedi Hasan <145797601+mehediinf@users.noreply.github.com>
2024-02-02 00:56:11 +0600

    First Commit SharedPreference_Storing_last_score

---
+++ app/src/main/java/com/example/sharedpreference_storing_last_score/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.sharedpreference_storing_last_score;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "SharedPreference_Storing_last_score"
+include(":app")
+ 
